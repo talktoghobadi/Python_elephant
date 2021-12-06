34917bb0f48d752d7adb7b745da2fb31e4b7687c
Maryam Ghobadi <talktoghobadi@gmail.com>
2021-12-06 06:34:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Name
+# 1/27/20
+# Unit 1 Final Practice Program 1
+
+print("Please be careful when around the \"baby\" elephant.")
+print("He \\ she is a very dangerous animal.")
+print("Thank you for your \'understanding.\'")
+
+print("""
+          _                 
+   ______/ \\-.   _ 
+.-/     (    o\\_//
+ |  ___  \\_/\\---\'
+ |_||  |_|| 
+
+""")
